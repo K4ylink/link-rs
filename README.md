4dbb2a8a43c759641cf66e2275a4f15baa4fbf42
CrLF0710 <crlf0710@gmail.com>
2016-07-10 23:35:04 +0800

    Add documentation and remove unimplemented methods.

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# link-rs
-Link, a safe anti-ownership relation between Rust objects.
+# Link
+Link, an experimental safe anti-ownership relation between Rust objects.
+
+## The Idea
+Normally, rust advocates a ownership model, in which a strict high-level to
+low-level relationship between objects is decided at design time.
+
+However, sometimes you may want to do it another way, in which you can defer
+the ownership relation decision to a later stage.
+
+Using `Link`, you are able to do so directly.
+
+However, there are three points to remember before you start to use `Link`:
+
+1. You'll need to always refer those objects as Rc\<RefCell\<T\>\>,
+in this way, safety is guaranteed.
+
+2. You HAVE TO disconnect all `Link`s  before those objects went out of
+scope. You can, however, disconnect from either side.
+
+3. In the mutable borrow case, you can't trip around a `Link`. That will fail
+because in Rust you can't borrow a thing mutable twice.
+
+## How to use
+
+Add this to your Cargo.toml:
+```toml
+[dependencies]
+field-offset = "0.1"
+link = "0.1"
+```
+
+To use it:
+
+Declare the structs like:
+```Rust
+struct A {
+    pub data: u32,
+    pub link1: Link<A, B>,
+}
+struct B {
+    pub data: String,
+    pub link2: Link<B, A>,
+}
+```
+import the `offset_of!` macro from the "field-offset" crate:
+
+```Rust
+#[macro_use]
+extern crate field_offset as offset;
+
+let mut a = Rc::new(RefCell::new(A {
+            data: 42,
+            link1: Link::new(),
+        }));
+
+let mut b = Rc::new(RefCell::new(B {
+               data: "hello".to_owned(),
+               link2: Link::new(),
+           }));
+Link::connect(&mut a, offset_of!{A => link1}, &mut b, offset_of!{B => link2});
+
+println!("{}", a.borrow().link1.remote_owner().unwrap().data) // hello
+println!("{}", b.borrow().link2.remote_owner().unwrap().data) // 42
+
+```
+You can also use `remote_owner_mut()` to borrow the objects on the other side
+ of the link mutably.
+
+## References
+* The `field-offset` crate is used along with Link.
+* The idea is also inspired by the `lia` language.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+//! Link is used for linking objects together in a undetermistic way.
+//! It allows the linked objects to last as long as a connection exists.
+//!
+//! However it is your duty to explicitly disconnect the links.
+
 #[macro_use]
 extern crate field_offset as offset;
 
 use std::ptr;
-use std::mem;
 use std::{rc, cell};
 
 struct LinkData<LocalT, TargetT> {
@@ -11,6 +15,7 @@ struct LinkData<LocalT, TargetT> {
     target_offset: offset::FieldOffset<TargetT, Link<TargetT, LocalT>>,
 }
 
+/// The Link object that represents a link between two structures.
 pub struct Link<LocalT, TargetT> {
     data: Option<LinkData<LocalT, TargetT>>,
 }
@@ -22,6 +27,41 @@ impl<LocalT, TargetT> Default for Link<LocalT, TargetT> {
 }
 
 impl<LocalT, TargetT> Link<LocalT, TargetT> {
+    /// Connect two `Link` objects inside two different mutably borrowed structures.
+    /// # Examples
+    ///
+    /// ```
+    /// # #[macro_use]
+    /// # extern crate field_offset as offset;
+    /// # extern crate link;
+    /// # use link::Link;
+    /// # use std::rc::Rc;
+    /// # use std::cell::RefCell;
+    /// struct A {
+    ///     pub data: u32,
+    ///     pub link1: Link<A, B>,
+    /// }
+    ///
+    /// struct B {
+    ///     pub data: String,
+    ///     pub link2: Link<B, A>,
+    /// }
+    ///
+    /// # fn main() {
+    /// let mut a = Rc::new(RefCell::new(A {
+    ///     data: 42,
+    ///     link1: Link::new(),
+    /// }));
+    ///
+    /// let mut b = Rc::new(RefCell::new(B {
+    ///     data: "hello".to_owned(),
+    ///     link2: Link::new(),
+    /// }));
+    ///
+    /// Link::connect(&mut a, offset_of!{A => link1}, &mut b, offset_of!{B => link2});
+    /// # }
+    /// ```
+
     pub fn connect(first_obj: &mut rc::Rc<cell::RefCell<LocalT>>,
                    first_offset: offset::FieldOffset<LocalT, Link<LocalT, TargetT>>,
                    second_obj: &mut rc::Rc<cell::RefCell<TargetT>>,
@@ -47,14 +87,17 @@ impl<LocalT, TargetT> Link<LocalT, TargetT> {
         }
     }
 
+    /// Create a unconnected `Link' object.`
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn is_null(&self) -> bool {
-        self.data.is_none()
+    /// Check if the `Link' object is connected.`
+    pub fn connected(&self) -> bool {
+        self.data.is_some()
     }
 
+    /// Borrow the object on this side if connected.
     pub fn owner(&self) -> Option<&LocalT> {
         if let Some(data) = self.data.as_ref() {
             unsafe { Some(data.offset.unapply(self)) }
@@ -63,6 +106,7 @@ impl<LocalT, TargetT> Link<LocalT, TargetT> {
         }
     }
 
+    /// Borrow the object on this side mutably if connected.
     pub fn owner_mut(&mut self) -> Option<&mut LocalT> {
         let self_mut_ptr = self as *mut _;
         if let Some(data) = self.data.as_ref() {
@@ -80,6 +124,7 @@ impl<LocalT, TargetT> Link<LocalT, TargetT> {
         self.owner_mut().map_or(ptr::null_mut(), |r| r as *mut _)
     }
 
+    /// Borrow the object on the other side.
     pub fn remote_owner(&self) -> Option<&TargetT> {
         if let Some(data) = self.data.as_ref() {
             unsafe { Some((&*(*data.target_obj).borrow() as *const _).as_ref().unwrap()) }
@@ -88,6 +133,7 @@ impl<LocalT, TargetT> Link<LocalT, TargetT> {
         }
     }
 
+    /// Borrow the object on the other side mutably.
     pub fn remote_owner_mut(&mut self) -> Option<&mut TargetT> {
         if let Some(data) = self.data.as_ref() {
             unsafe { Some((&mut *(*data.target_obj).borrow_mut() as *mut _).as_mut().unwrap()) }
@@ -104,22 +150,8 @@ impl<LocalT, TargetT> Link<LocalT, TargetT> {
         self.remote_owner_mut().map_or(ptr::null_mut(), |r| r as *mut _)
     }
 
-    pub fn swap(&mut self, other: &mut Self) {
-        {
-            let self_is_null = self.is_null();
-            let other_is_null = other.is_null();
-            if self_is_null && other_is_null {
-                return;
-            }
-            assert!(!self_is_null && !other_is_null);
-            assert!(self as *mut _ != other as *mut _);
-        }
-        mem::swap(&mut self.data.as_mut().unwrap().target_obj,
-                  &mut other.data.as_mut().unwrap().target_obj);
-        mem::swap(&mut self.data.as_mut().unwrap().target_offset,
-                  &mut other.data.as_mut().unwrap().target_offset);
-    }
 
+    /// Disconnect the `Link' object if it is connected.
     pub fn disconnect(&mut self) {
         if let Some(data) = self.data.as_mut() {
             let mut target_mut = data.target_obj
@@ -154,27 +186,37 @@ mod tests {
             data: 42,
             link: Link::new(),
         }));
-        assert!(a.borrow().link.is_null());
+        assert!(!a.borrow().link.connected());
         if true {
             let mut b = Rc::new(RefCell::new(B {
                 data: "hello".to_owned(),
                 link: Link::new(),
             }));
             Link::connect(&mut a, offset_of!{A => link}, &mut b, offset_of!{B => link});
-            assert!(!a.borrow().link.is_null());
-            assert!(!b.borrow().link.is_null());
+            assert!(a.borrow().link.connected());
+            assert!(b.borrow().link.connected());
             assert_eq!(a.borrow().link.owner_ptr(), &*a.borrow() as *const A);
             assert_eq!(b.borrow().link.owner_ptr(), &*b.borrow() as *const B);
+            assert_eq!(a.borrow().link.remote_owner_ptr(), &*b.borrow() as *const B);
+            assert_eq!(b.borrow().link.remote_owner_ptr(), &*a.borrow() as *const A);
+            assert_eq!(a.borrow().link.remote_owner().unwrap().data,
+                       "hello".to_owned());
+            assert_eq!(b.borrow().link.remote_owner().unwrap().data, 42);
             a.borrow_mut().link.disconnect();
-            assert!(a.borrow().link.is_null());
-            assert!(b.borrow().link.is_null());
+            assert!(!a.borrow().link.connected());
+            assert!(!b.borrow().link.connected());
             Link::connect(&mut b, offset_of!{B => link}, &mut a, offset_of!{A => link});
-            assert!(!a.borrow().link.is_null());
-            assert!(!b.borrow().link.is_null());
+            assert!(a.borrow().link.connected());
+            assert!(b.borrow().link.connected());
             assert_eq!(a.borrow().link.owner_ptr(), &*a.borrow() as *const A);
             assert_eq!(b.borrow().link.owner_ptr(), &*b.borrow() as *const B);
+            assert_eq!(a.borrow().link.remote_owner_ptr(), &*b.borrow() as *const B);
+            assert_eq!(b.borrow().link.remote_owner_ptr(), &*a.borrow() as *const A);
+            assert_eq!(a.borrow().link.remote_owner().unwrap().data,
+                       "hello".to_owned());
+            assert_eq!(b.borrow().link.remote_owner().unwrap().data, 42);
             b.borrow_mut().link.disconnect();
         }
-        assert!(a.borrow().link.is_null());
+        assert!(!a.borrow().link.connected());
     }
 }
